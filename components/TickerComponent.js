bbd5c47aa63ed16dd50ec76033545e9ba74863be
ChuckHagy <chuckhagy@gmail.com>
2017-09-20 13:37:25 -0700

    time countdown makes sense

--- components/TickerComponent.js
+++ components/TickerComponent.js
@@ -1,10 +1,12 @@
 import React from "react";
 import moment from "moment";
 import { Text } from "native-base";
+import { StyleSheet } from "react-native";
 
 export default class TickerComponent extends React.Component {
   state = {
-    timeRemaining: ""
+    timeRemaining: "",
+    math: ""
   };
 
   componentDidMount() {
@@ -22,21 +24,50 @@ export default class TickerComponent extends React.Component {
 
     let currentTime = moment(Date.now());
     let diff = moment(this.props.info.duedate).diff(currentTime, "seconds");
-    if (diff <= 60) this.setState({ timeRemaining: `${diff} sec` });
+    if (diff <= 60) this.setState({ timeRemaining: `${diff} sec`, math: diff });
     else if (diff <= 3600)
-      this.setState({ timeRemaining: `${(diff / 60).toFixed(2)} min` });
+      this.setState({
+        timeRemaining: `${(diff / 60).toFixed(2)} min`,
+        math: diff
+      });
     else if (diff <= 86400)
-      this.setState({ timeRemaining: `${(diff / 3600).toFixed(1)} hr` });
+      this.setState({
+        timeRemaining: `${(diff / 3600).toFixed(1)} hr`,
+        math: diff
+      });
     else if (diff >= 86400)
-      this.setState({ timeRemaining: `${(diff / 86400).toFixed(1)} day` });
+      this.setState({
+        timeRemaining: `${(diff / 86400).toFixed(1)} day`,
+        math: diff
+      });
     else this.setState({ timeRemaining: diff });
   }
 
   render() {
+    //colorLogic
+    let thisStyle = "";
+    if (this.state.math <= 30) thisStyle = styles.thirty;
+    else if (this.state.math <= 60) thisStyle = styles.sixty;
+    else thisStyle = this.props.style;
+
     return (
-      <Text style={this.props.style}>
+      <Text style={thisStyle}>
         {this.state.timeRemaining}
       </Text>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  sixty: {
+    color: "red",
+    fontSize: 20,
+    textAlign: "left"
+  },
+  thirty: {
+    color: "red",
+    fontSize: 20,
+    textAlign: "left",
+    fontWeight: "bold"
+  }
+});
