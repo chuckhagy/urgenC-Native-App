cb1bbc030692015b199ed171a8ef4cfbd25f855f
ChuckHagy <chuckhagy@gmail.com>
2017-10-11 14:44:47 -0700

    prepped for Q3 refactor

---
+++ components/LoginPageComponent.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import LoginPageLayout from "./LoginPageComponent";
+import {
+    Container,
+    Header,
+    Title,
+    Content,
+    Footer,
+    FooterTab,
+    Button,
+    Left,
+    Right,
+    Body,
+    Icon,
+    Text
+} from "native-base"
+
+export default class LoginPageComponent extends React.Component {
+    render(){
+        reutrn(
+
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+
+})
