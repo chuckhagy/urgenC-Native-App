5b71e001b87697598db2cfc6214f966ee8486667
ChuckHagy <chuckhagy@gmail.com>
2017-10-18 08:37:01 -0700

    removed redundant ticker code

--- components/TickerComponent.js
+++ components/TickerComponent.js
@@ -27,24 +27,12 @@ export default class TickerComponent extends React.Component {
         let diff = moment.utc(this.props.info.duedate).diff(currentTime, "seconds");
         let newDiff = moment(this.props.info.duedate).fromNow(true);
         if (diff <= 60) this.setState({timeRemaining: `${diff} sec`, math: diff});
-        else if (diff <= 3600) {
-
+        else{
             this.setState({
                 timeRemaining: newDiff,
                 math: diff
             });
-        } else if (diff <= 86400)
-            this.setState({
-                timeRemaining: newDiff,
-
-                math: diff
-            });
-        else if (diff >= 86400)
-            this.setState({
-                timeRemaining: newDiff,
-                math: diff
-            });
-        else this.setState({timeRemaining: diff});
+        }
     }
 
     render() {
